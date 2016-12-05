ce56453f9e22d804d13a0a91aa4259c3078625a5
sudolibre <optout.breaker@gmail.com>
2016-12-05 16:40:41 -0500

    Initial Commit

---
+++ TicTacToe/TicTacToe.h
@@ -0,0 +1,19 @@
+//
+//  TicTacToe.h
+//  TicTacToe
+//
+//  Created by Jonathon Day on 12/5/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TicTacToe.
+FOUNDATION_EXPORT double TicTacToeVersionNumber;
+
+//! Project version string for TicTacToe.
+FOUNDATION_EXPORT const unsigned char TicTacToeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TicTacToe/PublicHeader.h>
+
+
